b9f35be585f2c11a502892ce588269c754960ea2
brgcode <van.mele@arch.ethz.ch>
2021-06-28 00:13:01 +0200

    test scripts

---
+++ setup/tubemesh_rhino.py
@@ -0,0 +1,9 @@
+import compas
+from compas.datastructures import Mesh
+from compas_rhino.artists import MeshArtist
+
+mesh = Mesh.from_obj(compas.get('tubemesh.obj'))
+
+artist = MeshArtist(mesh)
+artist.draw_vertices()
+artist.draw_faces()
